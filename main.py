25ce8b9424ef6101cb3094e26494ecc552078a56
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2024-08-22 20:40:51 +0300

    добавил post запрос калькулятора первой версии

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ app = FastAPI()
 @app.get("/index")
 def hello():
     return FileResponse("index.html")
+
+@app.post("/calculation_v1")
+def calculation_v1(num_1, num_2):
+    return {"result": int(num_1) + int(num_2)}
+
+
