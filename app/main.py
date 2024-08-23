e6ce92d5b648a41b2bdd443fab98a31142ef84ee
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2024-08-23 21:24:38 +0300

    ТОДО

--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ def calculation_v1(num_1, num_2):
 
 @app.post("/create_user_v1")
 def create_user_v1(user: User):
-    return {"name": user.name}
+    return {"name": user.First_name + " " + user.Last_name}
--- app/models/schemas.py
+++ app/models/schemas.py
@@ -4,21 +4,27 @@ from typing import List, Union
 from pydantic import BaseModel,field_validator
 
 
-
-# создаём модель данных, которая обычно расположена в файле models.py
 class User(BaseModel):
     id: int
-    name: str = "John Doe"
+    First_name: str = "John"
+    Last_name: str = "Doe"
     signup_ts: Union[datetime, None] = None
     friends: List[int] = []
 
     @field_validator('id')
-    def check_age(cls, v):
+    def check_id(cls, v):
         if v < 0:
             raise ValueError('Нельзя присваивать полю id отрицательное значение')
         return v
 
-    @field_validator('name')
-    def check_name(cls, v):
-        if any(char.isdigit() for char in v):
-            raise ValueError('Name не может содержать цифры')
+# TODO: доделать валидацию
+    # @field_validator('First_name')
+    # def check_first_name(cls, v):
+
+    #     if (any(char.isdigit() for char in v)):
+    #         raise ValueError('First_name не может содержать цифры')
+
+    # @field_validator('First_name')
+    # def check_Last_name(cls, v):
+    #     if any(char.isdigit() for char in v):
+    #         raise ValueError('Last_name не может содержать цифры')
