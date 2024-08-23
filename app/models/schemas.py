1c32acbe561106f74fafb4e4bce9f15005f86e5c
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2024-08-23 21:06:48 +0300

    валидаторы

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import List, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel,field_validator
 
 
 
@@ -11,3 +11,14 @@ class User(BaseModel):
     name: str = "John Doe"
     signup_ts: Union[datetime, None] = None
     friends: List[int] = []
+
+    @field_validator('id')
+    def check_age(cls, v):
+        if v < 0:
+            raise ValueError('Нельзя присваивать полю id отрицательное значение')
+        return v
+
+    @field_validator('name')
+    def check_name(cls, v):
+        if any(char.isdigit() for char in v):
+            raise ValueError('Name не может содержать цифры')
