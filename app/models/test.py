d9a5d476719d1a3b6c35db6a98403b015cd66a9f
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2024-08-24 17:41:21 +0300

    add create Person

--- app/models/test.py
+++ app/models/test.py
@@ -1,1 +1,14 @@
-print('test')
+import re
+
+def is_valid_email(email: str) -> bool:
+    # Регулярное выражение для проверки email-адреса
+    email_regex = re.compile(
+        r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    )
+    return re.match(email_regex, email) is not None
+
+
+print(is_valid_email("a@a.ru"))
+print(is_valid_email("a@a.ff.fru "))
+print(is_valid_email("aa.ru.ru"))
+print(is_valid_email("@a.ru.ru"))
